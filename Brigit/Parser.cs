46966722f80a000749a5c4d4421ccc9a0b32e2f2
Erik Zapata <zapatabrandon@gmail.com>
2016-09-08 22:38:51 -1200

    Added some functions to the Eater class which will do an assorted amount of things like, travesring the txt file and returning snippets given some parameters

--- Brigit/Parser.cs
+++ Brigit/Parser.cs
@@ -14,4 +14,62 @@ namespace Brigit
 		    //
 	    }
     }
+
+    /// <summary>
+    /// A class that steps through 
+    /// </summary>
+    public class Eater
+    {
+        string data;
+        int pos;
+
+        /// <summary>
+        /// Peeks at the current char
+        /// </summary>
+        /// <returns>Returns the current char</returns>
+        public char SniffChar()
+        {
+            return data[pos];
+        }
+
+        /// <summary>
+        /// Steps forward by 1 in the string
+        /// </summary>
+        public void ConsumeChar()
+        {
+            pos++;
+        }
+
+        /// <summary>
+        /// Returns the current char and steps forward by one
+        /// </summary>
+        /// <returns>Returns the current char</returns>
+        public char SpitChar()
+        {
+            char c = SniffChar();
+            ConsumeChar();
+            return c;
+        }
+
+        /// <summary>
+        /// Func is pretty cool
+        /// </summary>
+        /// <param name="predicate"></param>
+        public void EatWhile(Func<Char, bool> predicate)
+        {
+            while(predicate(SniffChar()))
+            {
+                ConsumeChar();
+            }
+        }
+
+        /// <summary>
+        /// I hope I'm using this right
+        /// </summary>
+        public void EatWhiteSpace()
+        {
+            Func<char, bool> p = delegate (char c) { return Char.IsWhiteSpace(c); };
+            EatWhile(p);
+        }
+    }
 }
